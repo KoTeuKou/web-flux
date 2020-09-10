a72fd11e353543e4733bc4a427fc788233c46321
KoTeuKou <dima.lapushkin@mail.ru>
2020-09-10 17:37:42 +0400

    Initial commit

---
+++ src/main/java/com/flux/webflux/config/GreetingRouter.java
@@ -0,0 +1,22 @@
+package com.flux.webflux.config;
+
+import com.flux.webflux.handlers.GreetingHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.server.*;
+
+public class GreetingRouter {
+    @Bean
+    public RouterFunction<ServerResponse> route(GreetingHandler greetingHandler) {
+        RequestPredicate route = RequestPredicates
+                .GET("/hello")
+                .and(RequestPredicates.accept(MediaType.TEXT_PLAIN));
+
+        return RouterFunctions
+                .route(route, greetingHandler::hello)
+                .andRoute(
+                        RequestPredicates.GET("/"),
+                        greetingHandler::index
+                );
+    }
+}
